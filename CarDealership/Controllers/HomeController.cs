8dbda3ac084a32c0449d63d92610e3704b60209f
Andriy Veremyeyev <belyybrat@gmail.com>
2020-03-10 10:39:15 -0700

    Add a list, condition, create multiple controllers
    Co-authored-by: Adela Darmansyah <adela.yohana@gmail.com>

--- CarDealership/Controllers/HomeController.cs
+++ CarDealership/Controllers/HomeController.cs
@@ -1,16 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using CarDealership.Models;
 
 namespace CarDealership.Controllers
 {
-  public class HomeController: Controller
-  {
-    [Route("/")]
-
-    public ActionResult Index()
+    public class HomeController : Controller
     {
-    Car honda = new Car("CRV", 30, 2000, "2020");
-    return View(honda);
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+
     }
-  }
 }
--- CarDealership/Models/Car.cs
+++ CarDealership/Models/Car.cs
@@ -1,24 +1,35 @@
+using System.Collections.Generic;
+
 namespace CarDealership.Models
 {
   public class Car
-{
-  public string MakeModel;
-  public int Price;
-  public int Miles;
-  public string Message;
+  {
+    public string MakeModel { get; set; }
+    public int Price { get; set; }
+    public int Miles { get; set; }
+    public string Message { get; set; }
+    
+    private static List<Car> _models = new List<Car> {};
 
-  public Car(string makeModel, int price, int miles, string message)
-  {
-    MakeModel = makeModel;
-    Price = price;
-    Miles = miles;
-    Message = message;
-  }
-  public bool WorthBuying(int maxPrice, int maxMiles)
-  {
-    return (Price<=maxPrice && Miles<=maxMiles);
-  }
-} 
+    public Car(string makeModel, int price, int miles, string message)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+      Message = message;
+      _models.Add(this);
+    }
+
+    public static List<Car> GetAll()
+    {
+      return _models;
+    }
+
+    public bool WorthBuying(int maxPrice, int maxMiles)
+    {
+      return (Price<=maxPrice && Miles<=maxMiles);
+    }
+  } 
 }
 
 
